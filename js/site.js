ab3aafb5dba78f47dc17af1ae22c8e12a6aa6511
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2024-08-28 18:49:23 -0300

    correção js v2.0.2

--- js/site.js
+++ js/site.js
@@ -1,6 +1,10 @@
 
 const btn_menu = document.querySelector("#btn-menu");
 const btn_close = document.querySelector("#btn-close");
+const section = document.querySelectorAll('section');
+const links = document.querySelectorAll('.nav-item .nav-link');
+const navbar = document.querySelector('header');
+
 let currentIndex = 0;
 
 btn_menu.addEventListener("click", () => {
@@ -10,6 +14,24 @@ btn_menu.addEventListener("click", () => {
 btn_close.addEventListener("click", () => {
     document.querySelector(".navbar-collapse").classList.remove("collapse");
 });
+
+window.onscroll = () => {
+    section.forEach(s => {
+        let top = window.scrollY;
+        let offset = s.offsetTop - 150;
+        let height = s.offsetHeight;
+        let id = s.getAttribute('id');
+        let nav = navbar.clientHeight * 1.5;
+        
+        if(top >= offset && top < offset + height) {
+            links.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.navlist .nav-item a[href*='+ id +']').classList.add('active');
+                document.querySelector(".navbar-collapse").classList.remove("collapse");
+            });
+        }
+    });
+};
 
 function showSlide(index) {
     const slides = document.querySelectorAll('.carousel-item');
